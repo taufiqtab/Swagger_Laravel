d43413091c9eeaf58e0503dd14baefa0d2cdbc3d
taufiqtab <taufiqtab77@gmail.com>
2019-09-18 16:43:39 +0700

    init file

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\Http\Controllers;
+use http\Env\Response;
+use Illuminate\Http\Request;
+class UserController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/greet",
+     *     tags={"greeting"},
+     *     summary="WOI TESTING",
+     *     description="asdasdasd",
+     *     operationId="greet",
+     *     @OA\Parameter(
+     *          name="firstname",
+     *          description="nama depan",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Parameter(
+     *          name="lastname",
+     *          description="nama belakang",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="successful operation"
+     *     )
+     * )
+     */
+    public function greet(Request $request)
+    {
+        $userData = $request->only([
+            'firstname',
+            'lastname',
+        ]);
+        if (empty($userData['firstname']) && empty($userData['lastname'])) {
+            return new \Exception('Missing data', 404);
+        }
+        return 'Halo ' . $userData['firstname'] . ' ' . $userData['lastname'];
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/greet2",
+     *     tags={"greeting"},
+     *     summary="WOI TESTING 2",
+     *     description="asdasdasd",
+     *     operationId="greet",
+     *     @OA\Parameter(
+     *          name="firstname",
+     *          description="nama depan",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Parameter(
+     *          name="lastname",
+     *          description="nama belakang",
+     *          required=true,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="successful operation"
+     *     )
+     * )
+     */
+    public function greet2(Request $request)
+    {
+        $userData = $request->only([
+            'firstname',
+            'lastname',
+        ]);
+        if (empty($userData['firstname']) && empty($userData['lastname'])) {
+            return new \Exception('Missing data', 404);
+        }
+        return 'Halo ' . $userData['firstname'] . ' ' . $userData['lastname'];
+    }
+}
---
+++ app/Http/Swagger/swagger_models/settings/sample_controller.php
@@ -0,0 +1,8 @@
+<?php
+class SampleController //This is a sample laravel Controller
+{
+    
+    public function sampleFunctionWithDoc()
+    {
+    }
+}
